09b210ed9542d750bf504ce7d305d812e0978ccc
CavadRamazanzade <ramaz.vb00@gmail.com>
2025-04-10 23:01:07 +0400

    homework1.2

---
+++ homework1.2/homework1.2/homework1.2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	cout << "Enter num1: ", cin >> num1;
+	cout << "Enter num2: ", cin >> num2;
+	cout << "Enter num3: ", cin >> num3;
+	cout << num1 + num2+ num3 << endl;
+	cout << num1 * num2* num3 << endl;
+	cout << (num1 + num2 + num3) / 3 << endl;
+}
