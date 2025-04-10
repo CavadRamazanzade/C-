375c9994004854359386b8c0ab424323c284dda7
CavadRamazanzade <ramaz.vb00@gmail.com>
2025-04-10 23:29:44 +0400

    homework1.3-1.4

---
+++ homework1.3/homework1.3/homework1.3.cpp
@@ -0,0 +1,23 @@
+
+//Abulqasim Mellim ya ochen dolgo dumal nad etim zadaniem i kotoroe idet posle etogo no ni kak ne mogu sdelat ya 2 
+//chasa siju dumayu no ni kak .
+
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	cout << "Enter num1: ", cin >> num1;
+	cout << "Enter num2: ", cin >> num2;
+	cout << "Enter num3: ", cin >> num3;
+	cout << num1 % num3 << endl;
+	cout << num1 % num3 << endl;
+	cout << num1 << " cost  " << num2 << " quantity " << num3 << " percentage "; 
+
+}
