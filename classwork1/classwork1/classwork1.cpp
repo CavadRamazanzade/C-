1b3a2cb2c06654702bc85df842c5ef9da14cc235
CavadRamazanzade <ramaz.vb00@gmail.com>
2025-04-10 22:43:45 +0400

    classwork1

---
+++ classwork1/classwork1/classwork1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+
+
+	/*int num = 7;
+	bool test = num == 8;
+
+	if (num == 8)
+	{
+		cout << "OK" << endl;
+	}
+	else {
+		cout << "ERROR" << endl;
+	}
+	
+} */
