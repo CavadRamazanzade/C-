8c8ac8ade6b2d9c7e7521d74da6b994271433e6a
CavadRamazanzade <ramaz.vb00@gmail.com>
2025-04-16 18:49:30 +0400

    dz№2.1.2.3

---
+++ homework2.1/homework2.1/homework2.1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+//Abulqasim Mellim chestno skaju ya ne snog sdelat dz ono bilo ochen trudnim
+// ya smog prosto zapisat dla 2 zadaniya perevod chasa k minutam i sekundam
+
+
+int main()
+{
+	/*int num = 0;
+	cout << "Enter number: ", cin >> num;
+	int hour = num / 3600;
+	int minutes = (num - (hour * 3600)) / 60;
+	int seconds = (num - (hour * 3600)) - (minutes * 60);
+	cout << hour << " h " << minutes << " m " << seconds << 
+		if (num=2)
+		{
+			cout << num1 << endl;*/
+		}
+}
+
